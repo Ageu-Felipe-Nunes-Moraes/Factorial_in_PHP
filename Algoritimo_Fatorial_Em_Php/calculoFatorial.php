8ca649ff41cb51197ea158c7c5eff325eb0efaa5
Ageu Moraes <ageumoraes67@gmail.com>
2024-04-21 13:23:03 -0300

    Comentei todo o código

--- Algoritimo_Fatorial_Em_Php/calculoFatorial.php
+++ Algoritimo_Fatorial_Em_Php/calculoFatorial.php
@@ -5,37 +5,39 @@ class CalculaFatorial{
 
     private $fatorial, $resultado;
 
-    public function __construct() {
-        $this->fatorial = 0;
-        $this->resultado = 1;
+    public function __construct() { // Método construtor
+        $this->fatorial = 0;  // Vai receber o número dado pelo usuário
+        $this->resultado = 1; // Variável contadora que armazenará o resultado
     }
     
-    public function getValor(){
+    public function getValor(){ // Captura o valor dado pelo usuário
         echo "Digite um valor para descobrir o fatorial do mesmo: ";
         $this->fatorial = (int) readline();
         return $this->fatorial;
         }
     
-    public function verificaCondicoes(){
-        if ($this->fatorial < 0){
+    public function verificaCondicoes(){ // Verifica a condição necessária para ser considerado fatorial
+        if ($this->fatorial < 0){ // Se um valor negativo for inserido, vai aparecer essa mensagem
             echo "\nValor negativo, portanto invalido. \n";
         } else{
-            $this->imprimeResultado();
+            $this->imprimeResultado(); // Se não, o código chama a implementação da lógica e resultado
         }
     }
     
-    public function imprimeResultado(){
+    public function imprimeResultado(){ // Processa o valor inserido e entrega o resultado
+        // Enquanto i <= fatorial ele será múltiplicado com a variável "Resultado"
+        // EXEMPLO: 5! = 1*1 = 1*2 = 2*3 = 6*4 = 24 * 5 = 120
         for ($i = 1; $i <= $this->fatorial; $i++){
             $this->resultado *= $i;
         }
-        echo "O resultado de $this->fatorial em fatorial e: $this->resultado\n ";
+        echo "O resultado de $this->fatorial em fatorial e: $this->resultado\n "; // Resultado final
     }
-};
+}
     
     
-$calculadora = new CalculaFatorial();
-$calculadora->getValor();
-$calculadora->verificaCondicoes();
+$calculadora = new CalculaFatorial(); // Objeto 1
+$calculadora->getValor(); // Capturando valor do usuário
+$calculadora->verificaCondicoes(); // Verificando se o valor inserido é válido
 
 
 //Forma simplificada do código
