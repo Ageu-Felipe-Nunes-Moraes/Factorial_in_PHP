52a15ab58d78f9018e658fd39b3f1e94fc48ecc3
Ageu Moraes <ageumoraes67@gmail.com>
2024-04-23 14:19:03 -0300

    Melhorando a acessibilidade do código

--- Algoritimo_Fatorial_Em_Php/calculoFatorial.php
+++ Algoritimo_Fatorial_Em_Php/calculoFatorial.php
@@ -1,57 +1,57 @@
 
 <?php
 
-class CalculaFatorial{
+class FactorialCalculate{
 
-    private $fatorial, $resultado;
+    private $factorial, $result;
 
-    public function __construct() { // Método construtor
-        $this->fatorial = 0;  // Vai receber o número dado pelo usuário
-        $this->resultado = 1; // Variável contadora que armazenará o resultado
+    public function __construct() { // Construct method
+        $this->factorial = 0;  // Will receive number entered by the user
+        $this->result = 1; // Counter that will store the result
     }
     
-    public function getValor(){ // Captura o valor dado pelo usuário
+    public function getValue(){ // to get value entered by the user
         echo "Digite um valor para descobrir o fatorial do mesmo: ";
-        $this->fatorial = (int) readline();
-        return $this->fatorial;
+        $this->factorial = (int) readline();
+        return $this->factorial;
         }
     
-    public function verificaCondicoes(){ // Verifica a condição necessária para ser considerado fatorial
-        if ($this->fatorial < 0){ // Se um valor negativo for inserido, vai aparecer essa mensagem
+    public function checksConditions(){ // Checks the necessary conditions to have a factorial
+        if ($this->factorial < 0){ // If you enter negative value, it will show this message
             echo "\nValor negativo, portanto invalido. \n";
         } else{
-            $this->imprimeResultado(); // Se não, o código chama a implementação da lógica e resultado
+            $this->printResult(); // Otherwise, the code will call the logic implementation and the result
         }
     }
     
-    public function imprimeResultado(){ // Processa o valor inserido e entrega o resultado
-        // Enquanto i <= fatorial ele será múltiplicado com a variável "Resultado"
-        // EXEMPLO: 5! = 1*1 = 1*2 = 2*3 = 6*4 = 24 * 5 = 120
-        for ($i = 1; $i <= $this->fatorial; $i++){
-            $this->resultado *= $i;
+    public function printResult(){ // Processes entered value and delivers the result
+        // While i <= factorial, it will be multiplied by the "Result" variable
+        // EXEMPLE: 5! = 1*1 = 1*2 = 2*3 = 6*4 = 24 * 5 = 120
+        for ($i = 1; $i <= $this->factorial; $i++){
+            $this->result *= $i;
         }
-        echo "O resultado de $this->fatorial em fatorial e: $this->resultado\n "; // Resultado final
+        echo "O resultado de $this->factorial em fatorial e: $this->result\n "; // Final result
     }
 }
     
     
-$calculadora = new CalculaFatorial(); // Objeto 1
-$calculadora->getValor(); // Capturando valor do usuário
-$calculadora->verificaCondicoes(); // Verificando se o valor inserido é válido
+$calculator = new FactorialCalculate(); // Object 1
+$calculator->getValue(); // To get user value
+$calculator->checksConditions(); // Checks if the entered value is valid
 
 
-//Forma simplificada do código
+//Simple form of the code
 /* 
-$fatorial = 1;
+$factorial = 1;
 echo "Digite um valor para descobrir o fatorial do mesmo: ";
 
-$resultado = readline();
+$result = readline();
 
-if ($resultado > 0){
-    for ($i = 1; $i <= $resultado; $i++){
-        $fatorial *= $i;
+if ($result > 0){
+    for ($i = 1; $i <= $result; $i++){
+        $factorial *= $i;
     }
-    echo "O fatorial de $resultado é: $fatorial \n";
+    echo "O fatorial de $result é: $factorial \n";
 } else{
     echo "O valor é inválido";
 }
